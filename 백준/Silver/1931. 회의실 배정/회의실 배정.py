22ef97ca68e28f5b0427ebe1c90c97c41f4f70f0
HYEIN KIM <122677410+hyein604@users.noreply.github.com>
2024-07-26 21:49:35 +0900

    [Silver I] Title: 회의실 배정, Time: 504 ms, Memory: 127880 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,13 @@
+n = int(input())
+time_table = sorted(list(list(map(int, input().split())) for _ in range(n)))
+# time_table.sort(key = lambda x: (x[1], x[0]))
+time_table.sort(key = lambda x: (x[1]))
+
+cnt = 1
+end_time = time_table[0][1]
+for i in range(1, n):
+    if time_table[i][0] >= end_time:
+        cnt += 1
+        end_time = time_table[i][1]
+
+print(cnt)
