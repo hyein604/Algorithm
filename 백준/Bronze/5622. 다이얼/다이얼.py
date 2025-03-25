d1b0bd5f6b49ae71ce600c64562e0ab96086d93c
HYEIN KIM <122677410+hyein604@users.noreply.github.com>
2025-03-25 11:22:19 +0900

    [Bronze II] Title: 다이얼, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -3,13 +3,8 @@ dial_dic = {'A':2, 'B':2, 'C':2, 'D':3, 'E':3, 'F':3, 'G':4, 'H':4, 'I':4, 'J':5
 
 # 문자열 -> 숫자
 dial = input()
-dial_int = ''
-for dial_str in dial:
-    dial_int += str(dial_dic[dial_str])
-
-# 숫자 -> 시간
 answer = 0
-for num in dial_int:
-    answer += int(num) + 1
+for dial_str in dial:
+    answer += dial_dic[dial_str] + 1
 
 print(answer)
