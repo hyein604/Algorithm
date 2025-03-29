08ed4c0e366ad2ec469d9e8c6ecb20c401079259
HYEIN KIM <122677410+hyein604@users.noreply.github.com>
2025-03-30 01:58:43 +0900

    [Silver IV] Title: 설탕 배달, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,15 @@
+n = int(input())
+answer = 0
+
+while n > 0:
+    if n % 5 == 0:
+        answer += n // 5
+        n = 0
+        continue
+    n -= 3
+    answer += 1
+
+if n == 0:
+    print(answer)
+else:
+    print(-1)
