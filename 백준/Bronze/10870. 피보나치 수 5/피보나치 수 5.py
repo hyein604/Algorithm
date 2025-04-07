802e09b66ce7824de028bb693813b2564d81c681
HYEIN KIM <122677410+hyein604@users.noreply.github.com>
2025-04-07 17:47:20 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,21 +1,26 @@
-N=int(input())
+n = int(input())
+
 num1 = 0
 num2 = 1
 answer = 0
-def recur(count):
+
+def recur(cnt):
     global num1, num2, answer
-    if count == N and N != 1:
+
+    if cnt == n and n != 1:
         print(answer)
         return
-    if N == 0:
+
+    if n == 0:
         print(0)
         return
-    if N == 1:
+    if n == 1:
         print(1)
         return
+
     answer = num1 + num2
     num1 = num2
     num2 = answer
-    recur(count+1)
+    recur(cnt + 1)
 
 recur(1)
